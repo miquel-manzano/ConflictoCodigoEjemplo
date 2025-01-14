08ac2876add0f0f091fed23c25ba7c3a752142b5
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-01-14 19:50:46 +0100

    Implementacion metodo de comprobacion de senar o par.

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -2,6 +2,11 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Buenos diaaaas!!");
+        int num = 2;
+        Console.WriteLine(IsEven(num));
+    }
+    public static bool IsEven(int num)
+    {
+        return num % 2 == 0;
     }
 }

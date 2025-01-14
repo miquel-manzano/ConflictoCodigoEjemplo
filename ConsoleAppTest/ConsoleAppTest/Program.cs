a9004ddff75565d92ef5b7dd4b4872ddad019e97
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-01-14 19:57:25 +0100

    Implementacion metodo preguntar numero a usuario.

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -2,6 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Buenos diaaaas!!");
+        Console.WriteLine("Escribe un numero:");
+        int userNum = UserIntImput();
+        Console.WriteLine($"Tu numero es: {userNum}");
+    }
+
+    public static int UserIntImput()
+    {
+        return int.Parse(Console.ReadLine());
     }
 }
